ffb7833da9bf24c323ce6af0b1ef2af9536f294f
zuyatna <zuyatna@gmail.com>
2025-07-11 23:29:09 +0700

    update account model

--- e-money-service/internal/model/account.go
+++ e-money-service/internal/model/account.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Account struct {
-	ID        string         `gorm:"primaryKey"`
-	UserID    string         `gorm:"uniqueIndex"`
-	Balance   float64        `gorm:"default:0"`
-	CreatedAt time.Time      `gorm:"autoCreateTime"`
-	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID           string `gorm:"primaryKey"`
+	Username     string `gorm:"uniqueIndex"`
+	PasswordHash string
+	Balance      float64        `gorm:"default:0"`
+	CreatedAt    time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
 }
